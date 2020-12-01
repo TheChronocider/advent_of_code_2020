58744ef56f22d062419ae81dbbeab8e6291e5f19
Rogalewicz <Zane.Rogalewicz@act.gov.au>
2020-12-01 15:49:27 +1100

    Completed solution for part 2 of day 1

--- 2019/day_1/day_1_sln.py
+++ 2019/day_1/day_1_sln.py
@@ -7,19 +7,48 @@ Created on Tue Dec  1 14:16:43 2020
 
 import math
 
-with open('input.txt') as f:
-    data = f.readlines()
+def part_1():
+    with open('input.txt') as f:
+        data = f.readlines()
+        
+        ans = 0
+        
+        for val in data:
+            try:
+                #print(val)
+                result = int(math.floor(float(val) / 3.0) - 2.0)
+                #print (result)
+                ans += result
+            except:
+                print('error')
+        f.close()
+        return (ans)
+    return 0
+
+def part_2():
+    with open('input.txt') as f:
+        data = f.readlines()
+        
+        ans = 0
+        
+        for val in data:
+            try:
+                result = int(math.floor(float(val) / 3.0) - 2.0)
+                ans += part_2_a(result)
+            except:
+                print('error')
+        f.close()
+        return (ans)
+    return 0
     
-    ans = 0
+def part_2_a(fuel_req):
     
-    for val in data:
-        try:
-            #print(val)
-            result = int(math.floor(float(val) / 3.0) - 2.0)
-            #print (result)
-            ans += result
-        except:
-            print('error')
-    print (ans)
-    f.close()
+    if fuel_req <= 0:
+        return 0
+    ans = int(math.floor(float(fuel_req) / 3.0) - 2.0)
+    
+    return fuel_req + part_2_a(ans)
+        
+
+print(part_2())
     
