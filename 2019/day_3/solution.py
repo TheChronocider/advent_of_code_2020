e17a342aeb3156715ae696da0fddae2d6bff2b6b
Rogalewicz <Zane.Rogalewicz@act.gov.au>
2020-12-04 09:30:46 +1100

    Updated more of 2019 day 3. I am either doing this really stupidly or this is a really difficult question. Attempting to calculate line intersects or horizontal and verticle lines

--- 2019/day_3/solution.py
+++ 2019/day_3/solution.py
@@ -1,12 +1,29 @@
+
 class line:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
 
     def intersect(self, line1):
+        #i1 = [min(self.p1.x, self.p2.x),max(self.p1.x, self.p2.x)]
+        #i2 = [min(line1.p1.x, line1.p2.x),max(line1.p1.x, line1.p2.x)]
+        
+        #iA = [max(min(self.p1.x, self.p2.x),min(line1.p1.x, line1.p2.x)),
+        #      min(max(self.p1.x, self.p2.x),max(line1.p1.x, line1.p2.x))]
+        
+        #if (max(self.p1.x, self.p2.x) < min(line1.p1.x, line1.p2.x)):
+        #    return False
+        
+        x1, x2, x3, x4 = [self.p1.x, self.p2.x, line1.p1.x, line1.p2.x]
+        y1, y2, y3, y4 = [self.p1.y, self.p2.y, line1.p1.y, line1.p2.y]
+        
+        
+            
+        
+        
         return False
 
-    def int_point(self, line1):
+    def int_point(self, line1):      
         return None
 
 class point:
@@ -47,7 +64,29 @@ def part_1():
                 
                 p = point(x,y)
                 
-                points_1 = points_1 ++ [p]
+                # Store new line in list
+                lines_1.append(line(points_1[-1], p))
+                # Store new point in list
+                points_1.append(p)
+                
+            for val in line_2:
+                x, y = new_coord(x,y,val[0],val[1])
+                
+                p = point(x,y)
+                
+                # Store new line in list
+                lines_2.append(line(points_1[-1], p))
+                # Store new point in list
+                points_2.append(p)
+            
+            intersects = 0
+            
+            for line1 in lines_1:
+                for line2 in lines_2:
+                    if line1.intersect(line2):
+                        intersects += 1
+            
+            return intersects
                 
     #except:
     #    print("error")
