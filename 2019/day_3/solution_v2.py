bb89212ba0c59369ae52f66063e1498e16874aab
Rogalewicz <Zane.Rogalewicz@act.gov.au>
2020-12-04 15:09:40 +1100

    I Swear I'm going crazygit add *!

--- 2019/day_3/solution_v2.py
+++ 2019/day_3/solution_v2.py
@@ -16,36 +16,36 @@ class Point:
     
 
 class Line:
+    
     # A line between two points
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
+        
+        self.horizontal = (p1.x == p2.x)
     
     # Get intersections with another line
     def get_intersections(self, line1):
         # Line either horizontal or vertical
         # Line either intersects once or multiple times
         
-        xdiff = (self.p1.x - self.p2.x, line1.p1.x - line1.p2.x)
-        ydiff = (self.p1.y - self.p2.y, line1.p1.y - line1.p2.y)
+        # Getting x and y values stored as simple variables
+        x1, x2, x3, x4 = [self.p1.x, self.p2.x, line1.p1.x, line1.p2.x]
+        y1, y2, y3, y4 = [self.p1.y, self.p2.y, line1.p1.y, line1.p2.y]
+        
+        if self.horizontal and not line1.horizontal:
+            if y1 <= max(y3, y4) and y1 >= min(y3, y4) and x3 <= max(x1,x2) and x3 >= min(x1, x2):
+                return x3, y1
+        elif line1.horizontal and not self.horizontal:
+            if x1 <= max(x3, x4) and x1 >= min(y3,y4) and y3 <= max(y1, y2) and y3 >= min(y1, y2):
+                return x1, y3
         
         
-        def det(a, b):
-            return a[0] * b[1] - a[1] * b[0]
         
-        div = det(xdiff, ydiff)
-        
-        if div == 0:
-            return None
-        
-        
-        d = (det(*[[self.p1.x, self.p1.y],[self.p2.x, self.p2.y]]),
-             det(*[[line1.p1.x, line1.p1.y],[line1.p2.x, line1.p2.y]]))
-        
-        x = det(d, xdiff) / div
-        y = det(d, ydiff) / div
-        
-        return x,y
+        return None
+    
+    def print_line(self):
+        print(self.p1.x, self.p1.y, self.p2.x, self.p2.y)
         
     
 def inc_position(x, y, direction, steps):
@@ -96,17 +96,25 @@ def part_1():
                 prevPoint = newPoint
             
             lines.append(newLines)
+            
         
         intersects = set()
-        
         
         for line1 in lines[0]:
             for line2 in lines[1]:
                 intersect = line1.get_intersections(line2)
+                
                 if not(intersect == None):
+                    print("intersect:", intersect)
+                    print("Line 1:")
+                    print (line1.p1.x, line1.p1.y)
+                    print (line1.p2.x, line1.p2.y)
+                    print("Line 2:")
+                    print (line2.p1.x, line2.p1.y)
+                    print (line2.p2.x, line2.p2.y)
                     intersects.add(intersect)
         
-        print(sorted(intersects))
+        #print(sorted(intersects))
         return sum(1 for coord in intersects)
                 
         
